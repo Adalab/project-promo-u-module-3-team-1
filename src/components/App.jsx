ee7fd52b7cc0964d808e89802aa0289e02260630
Tania <taniie_bcn@hotmail.es>
2023-10-10 14:01:00 +0200

    validations autor&job

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 // Fichero src/components/App.jsx
 import '../styles/App.scss';
 import cover from '../images/cover.jpeg';
-import cover2 from '../images/cover_2.jpeg';
+//import cover2 from '../images/cover_2.jpeg';
 import logo from '../images/logo-adalab.png';
 import user from '../images/user.jpeg';
 import { useState } from 'react';
-
 
 
 function App() {
@@ -17,35 +16,62 @@ function App() {
   const [desc, setDesc] = useState('');
   const [autor, setAutor] = useState('');
   const [job, setJob] = useState('');
-  const handleInput = (ev)=>{
+  let [error, setError] = useState('');
+  let regex = /^[A-Za-záéíóúÁÉÍÓÚüÜñÑ ]*$/;
+
+  const handleInput = (ev) => {
     ev.preventDefault();
-    const inputId = ev.target.id;
+      
+    const value = ev.target.value;
+    console.log(value);
+    // Utiliza una expresión regular para verificar si el valor contiene números
+    if (regex.test(value)) {
+      console.log('letras validas');
+      const inputId = ev.target.id;
     if (inputId === 'name') {
+      setError('');
       setName(ev.target.value);
-    }else if (inputId === 'slogan'){
+    } else if (inputId === 'slogan') {
+      setError('');
       setSlogan(ev.target.value);
-    }else if (inputId === 'repo'){
-        setRepo(ev.target.value);
-    }else if (inputId === 'demo'){
-      setDemo(ev.target.value);
-
-    }else if  (inputId === 'technologies'){
+    ///} else if (inputId === 'repo') {
+      //setRepo(ev.target.value);
+    //} else if (inputId === 'demo') {
+      //setDemo(ev.target.value);
+    } else if (inputId === 'technologies') {
+      setError('');
       setTecno(ev.target.value);
-
-    }else if (inputId === 'desc'){
+    } else if (inputId === 'desc') {
+      setError('');
       setDesc(ev.target.value);
-
-    }else if (inputId === 'autor'){
+    } else if (inputId === 'autor') {
+      if (regex.test(value)) {
+      setError('');
       setAutor(ev.target.value);
-
-    }else if (inputId === 'job'){
-      setJob(ev.target.value);
-
+    } else {
+      console.log('error');
+      setError('Este campo no admite números');
     }
-  }
+  }  else if (inputId === 'job') {
+    if (regex.test(value)) {
+      setError('');
+      setJob(ev.target.value);
+    } else {
+      console.log('error');
+      setError('Este campo no admite números');
+      ev.target.value = '';
+    }
+      setJob(ev.target.value);
+    }
+    } else {
+      console.log('error');
+      setError('Este campo no admite números');
+    }
     
-  
- 
+    
+    
+  };
+
   return (
     <div className="container">
       <header className="header">
@@ -54,7 +80,7 @@ function App() {
       </header>
       <main className="main">
         <section className="preview">
-          <img className="image" src={demo || cover} alt=""  />
+          <img className="image" src={cover} alt="" />
 
           <section className="autor">
             <section className="info-project">
@@ -64,7 +90,8 @@ function App() {
               <h2 className="title">{name || 'Elegant Workspace'}</h2>
               <p className="slogan">{slogan || 'Diseños Exclusivos'}</p>
               <p className="desc">
-               {desc ||  `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                {desc ||
+                  `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Libero, delectus Voluptates at hic aliquam porro ad suscipit
                 harum laboriosam saepe earum doloribus aperiam, ullam culpa
                 accusantium placeat odit corrupti ipsum`}
@@ -75,7 +102,7 @@ function App() {
             </section>
 
             <section className="info-autor">
-              <img className="image" src={user}alt="" />
+              <img className="image" src={user} alt="" />
               <p className="job">{job || 'Full Stack Developer'}</p>
               <p className="name">{autor || 'Emmelie Björklund'}</p>
             </section>
@@ -97,8 +124,9 @@ function App() {
               placeholder="Nombre del proyecto"
               name="name"
               id="name"
-onChange = {handleInput}
-
+              onChange={handleInput}
+              value={name}
+              
             />
             <input
               className="input"
@@ -106,7 +134,8 @@ onChange = {handleInput}
               name="slogan"
               id="slogan"
               placeholder="Slogan"
-onChange = {handleInput}
+              onChange={handleInput}
+              value={slogan}
             />
             <input
               className="input"
@@ -114,8 +143,8 @@ onChange = {handleInput}
               name="repo"
               id="repo"
               placeholder="Repo"
-onChange = {handleInput}
-
+              onChange={handleInput}
+              value={repo}
             />
             <input
               className="input"
@@ -123,7 +152,8 @@ onChange = {handleInput}
               placeholder="Demo"
               name="demo"
               id="demo"
-onChange = {handleInput}
+              onChange={handleInput}
+              value={demo}
             />
             <input
               className="input"
@@ -131,7 +161,8 @@ onChange = {handleInput}
               placeholder="Tecnologías"
               name="technologies"
               id="technologies"
-onChange = {handleInput}
+              onChange={handleInput}
+              value={tecno}
             />
             <textarea
               className="textarea"
@@ -139,7 +170,8 @@ onChange = {handleInput}
               placeholder="Descripción"
               name="desc"
               id="desc"
-onChange = {handleInput}
+              onChange={handleInput}
+              value={desc}
             ></textarea>
           </fieldset>
 
@@ -155,7 +187,9 @@ onChange = {handleInput}
               placeholder="Nombre"
               name="autor"
               id="autor"
-onChange = {handleInput}
+              onChange={handleInput}
+              value={autor}
+              pattern='/^[A-Za-záéíóúÁÉÍÓÚüÜñÑ ]*$/'
             />
             <input
               className="input"
@@ -163,8 +197,11 @@ onChange = {handleInput}
               placeholder="Trabajo"
               name="job"
               id="job"
-              onChange = {handleInput}
+              onChange={handleInput}
+              value={job}
+              pattern='/^[A-Za-záéíóúÁÉÍÓÚüÜñÑ ]*$/'
             />
+            <p>{error}</p>
           </fieldset>
 
           <section className="buttons-img">
@@ -172,24 +209,21 @@ onChange = {handleInput}
             <button className="btn">Subir foto de autora</button>
           </section>
           <section className="buttons-img">
-            <button className="btn-large">
-              Crear Tarjeta
-            </button>
+            <button className="btn-large">Crear Tarjeta</button>
           </section>
 
           <section className="card">
             <span className=""> La tarjeta ha sido creada: </span>
-            <a href="" className="" target="_blank" rel="noreferrer"> </a>
+            <a href="" className="" target="_blank" rel="noreferrer">
+              {' '}
+            </a>
           </section>
         </section>
       </main>
       <footer>
-
         <img src={logo} alt="" />
       </footer>
     </div>
   );
-
-  
 }
 export default App;
