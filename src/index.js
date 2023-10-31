d3f68197d8fa6502270a047bb38a34d8f8d0ed01
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-10-31 12:59:03 +0100

    server

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ const port = 3500;
 app.listen(port, () => {
   console.log(`ha arrancado mi server en http://localhost:${port}`);
 });
+app.get('/projects/list', (req, res) => {
+  const projects = [{ nombre: 'Alba' }, { nombre: 'Mireia' }];
+  res.json(projects);
+  res.send('Hola adalabers');
+});
+app.get('/', (req, res) => {
+  res.send('Hola adalabersss');
+});
+const staticServerPath = './web';
+app.use(express.static(staticServerPath));
