feb237573f4f7207863c80393dd43b0e28b0eadb
Tania <taniie_bcn@hotmail.es>
2023-11-02 13:49:49 +0100

    added endpoint

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 const app = express();
 app.use(cors());
+app.use (express.json ({limit:"25mb"}));
+
+async function getConnection() {
+  //creary configurar la conexion
+  const connection = await mysql.createConnection({
+    host: "sql.freedb.tech",
+    user: "freedb_admin project",
+    password: "GpgBz?d5W9jbcGc",
+    database: "freedb_proyectosMolones",
+  });
+  connection.connect();
+  return connection;
+}
+
 
 const port = 3500;
 app.listen(port, () => {
   console.log(`ha arrancado mi server en http://localhost:${port}`);
 });
-app.get('/projects/list', (req, res) => {
-  const projects = [{ nombre: 'Alba' }, { nombre: 'Mireia' }];
-  res.json(projects);
-  res.send('Hola adalabers');
+
+
+app.get('/authors/list', async (req, res) => {
+  const conn = await getConnection();
+  const queryAuthors = "SELECT * FROM autora, proyectos WHERE proyectos.fk_autora = autora.idAutora;";
+  const [results] = await conn.query(queryAuthors);
+  conn.end();
+
+  res.json(results);
 });
-app.get('/', (req, res) => {
-  res.send('Hola adalabersss');
-});
-const staticServerPath = './web';
+
+const staticServerPath = './web/dist/';
 app.use(express.static(staticServerPath));
