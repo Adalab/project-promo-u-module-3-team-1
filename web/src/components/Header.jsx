9ab3e2dde61a489b5866dd3a4141e7ee953083f4
Maria Laura Luraschi <loud.luraschi@gmail.com>
2023-11-15 20:21:04 +0100

    background color

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -2,24 +2,22 @@ import '../styles/index.scss';
 import logo from '../images/logo-adalab.png';
 import { Link } from 'react-router-dom';
 
-
-
 function Header() {
   return (
     <>
       <div className='header'>
-        <Link className='Link-style' to="/">
+        <Link className='Link-style' to='/'>
           <section className='header__section'>
             <i className='header__section--icon fa-solid fa-laptop-code'></i>
-            <p className='header__section--text'>Proyectos Molones</p>
+            <p className='header__section--text'>Tech Sisters</p>
           </section>
         </Link>
         <img className='header__logo' src={logo} alt='' />
       </div>
       <div className='header__presentation'>
-        <h1 className='header__presentation--h1'>Proyectos Molones</h1>
+        <h1 className='header__presentation--h1'>Tech Sisters</h1>
         <p className='header__presentation--p'>
-          Escaparate en línea para recoger ideas a través de la tecnología.
+          Mujeres líderes en la tecnología. Unidas somos más fuertes.
         </p>
       </div>
     </>
